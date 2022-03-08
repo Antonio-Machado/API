3439bc757a91c1046fb6085623e3ba5db896a744
Antonio Machado <antoniomachado__@hotmail.com>
2022-03-08 07:58:36 -0300

    fix node version

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 console.log("~url: ", process.env.DATABASE)
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.DATABASE ? process.env.DATABASE : "mongodb://localhost:27017/cursos_online")
 module.exports = mongoose.connection
